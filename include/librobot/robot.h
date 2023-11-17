6f88e031db83afc16ea98898962218d5306c1335
Yili Yu <yili.yu@richmond.edu>
2023-11-17 15:33:46 -0500

    update code revised for sprint 2 and move files properly

--- include/librobot/robot.h
+++ include/librobot/robot.h
@@ -7,7 +7,6 @@
 // Enum for Robot Size
 enum class Size {
     Small,
-    Medium,
     Large
 };
 
@@ -28,6 +27,11 @@ private:
 public:
     // Constructor
     Robot(Size robotSize, Type robotType, int processingTime, std::string loc);
+    Robot();
+
+    // Virtual methods
+    virtual int clean();
+    virtual ~Robot() = default;
 
     // Getter methods
     Size getSize();
@@ -36,10 +40,30 @@ public:
 
     // Setter methods
     void setTime(int processingTime);
+    void setSize(std::string input_size);
+    void setType(std::string input_type);
+    void setLoc(std::string input_loc);
 
     // Test Meth
     void print();
-    
+};
+
+class Mop: public Robot {
+    public:
+        Mop(Size robotSize, Type robotType, int processingTime, std::string loc_value);
+        int clean() override;
+};
+
+class Vacuum: public Robot {
+    public:
+        Vacuum(Size robotSize, Type robotType, int processingTime, std::string loc_value);
+        int clean() override;
+};
+
+class Sweeper: public Robot {
+    public:
+        Sweeper(Size robotSize, Type robotType, int processingTime, std::string loc_value);
+        int clean() override;
 };
 
 #endif // ROBOT_H
