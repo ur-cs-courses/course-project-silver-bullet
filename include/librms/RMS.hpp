447ea5a9e0dbf94916be21fb83372446d94b2fa2
Yili Yu <yili.yu@richmond.edu>
2023-12-01 15:51:11 -0500

    add datalog func

--- include/librms/RMS.hpp
+++ include/librms/RMS.hpp
@@ -6,6 +6,8 @@
 #include <tuple>
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <chrono>
 #include <unordered_map>
 #include <string>
 #include <mutex>
@@ -151,6 +153,7 @@ class RobotManagementSystem {
         }
 
         void simulation(){
+            std::ofstream outFile("robot_log.txt");
             while (true)
             {   
                 if (mtxflag) {
@@ -167,6 +170,7 @@ class RobotManagementSystem {
                             int fixtime = robots[rob.first] -> fixTime();
                             robots[rob.first] -> setTime(fixtime + 1);
                             robots[rob.first] -> setLoc("Maintenance Station");
+                            outFile << "Robot" << rob.first << " is broken at " << "Maintenance Station" << std::endl;
                         }
                         else{
                             dectime(rob.first);
@@ -174,6 +178,10 @@ class RobotManagementSystem {
                             if (checkFinish(rob.first)){
                                 busyRobot[rob.first] = false;
                                 robots[rob.first] -> setLoc("hub");
+                                outFile << "Robot" << rob.first << " is sent back to hub at time " << getTime() << std::endl;
+                            }
+                            else{
+                                outFile << "Robot" << rob.first << " is cleaning room " << robots[rob.first] -> getLoc() << " @ location at time " << getTime() << std::endl;
                             }
                         }                
                     }
@@ -186,12 +194,14 @@ class RobotManagementSystem {
                         if (checkFinish(rob.first)){
                             brokenRobot[rob.first] = false;
                             robots[rob.first] -> setLoc("hub");
+                            outFile << "Robot" << rob.first << " is fixed and sent back to hub at time " << getTime() << std::endl;
                         }
                     }
                 }
                 mtx.unlock();
-                std::this_thread::sleep_for(std::chrono::seconds(3));
+                std::this_thread::sleep_for(std::chrono::seconds(1));
             }
+            outFile.close();
         }
 
         void debug(){
