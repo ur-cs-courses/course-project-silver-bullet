50166a212c17369e2ccbbe3277aeaca6dc0dc506
Thando Tsela <91032983+ThandoTsela@users.noreply.github.com>
2023-11-30 19:33:15 -0500

    updated cmake lists
    updated cmake lists

--- tests/room_test.cpp
+++ tests/room_test.cpp
@@ -1,5 +1,8 @@
 // roomMain.cpp
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include "../include/libroom/room.hpp"
+
 
 #include <unordered_map>
 #include <iostream>
